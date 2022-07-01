0c655bf267cbf9d80997c6bd3d2e679de58a6c36
Oliver Krilov <sbosvk@gmail.com>
2022-07-01 11:57:50 +0200

    Added: Install with/for snap. Custom path

--- desktop-install.sh
+++ desktop-install.sh
@@ -8,8 +8,9 @@ blue="\e[0;94m"
 green="\e[0;92m"
 uline="\e[4m"
 reset="\e[0m"
+tab="    "
 
-echo -e "${blue}Installing to ${reset}${uline}/usr/bin folder${reset}${blue}, requires permissions...${reset}"
+echo -e "Installing to '${blue}/usr/bin/${reset}', requires permissions..."
 
 if [[ "$EUID" = 0 ]]; then
     echo -e "${green}Running as root${reset}"
@@ -24,45 +25,66 @@ fi
 # App name
 APP=digibyte-core
 
+#Current Path
+CURR_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
 # App path
-APP_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+echo -e "Where is the '${blue}DigiByte-Core${reset}' application installed?\n${tab}1:${tab}Current path ${blue}($CURR_PATH/)${reset}\n${tab}2:${tab}Snap install ${blue}(/snap/)${reset}\n${tab}3:${tab}Custom path"
+read PATH_SELECTION
+
+if [ $PATH_SELECTION == 1 ]; then
+        APP_PATH="$CURR_PATH"
+elif [ $PATH_SELECTION == 2 ]; then
+        if ! command -v snap &> /dev/null; then
+                echo "Snapcraft not installed, please install it and try again."
+                exit 1
+        else
+                if ! snap list | grep digibyte-core; then
+                        read -p "digibyte-core not found, install it now? (Y/n) " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+                        sudo snap install digibyte-core
+                        if snap list | grep digibyte-core; then
+                                APP_PATH="/snap/digibyte-core/current"
+                        else
+                                echo -e "${red}DigiByte-Core could not be found, was the installation aborted?${reset}"
+                                exit 1
+                        fi
+                fi
+        fi
+elif [ $PATH_SELECTION == 3 ]; then
+        read -p "Enter custom path: " CUSTOM_PATH
+        APP_PATH="${CUSTOM_PATH}"
+else
+        echo -e "${red}Invalid selection, exiting.${reset}"
+fi
+
+echo -e "Continue with path '${blue}${APP_PATH}${reset}'? (Y/n) "
+read confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 # Make app executable
-sudo ln -nsf $APP_PATH/digibyte-qt /usr/bin/$APP
+sudo ln -nsf $APP_PATH/bin/digibyte-qt /usr/bin/$APP
 echo ""
 
-## Image
-ICON=$APP_PATH/icon.png
-ICON_DIR=$APP_PATH/../desktop
-ICON_URL=https://github.com/Sbosvk/digibyte-desktop-install/raw/main/512x512-sphere.png
+# ICON
+ICON=icon.png
+ICON_DIR=$APP_PATH/icon/
+ICON_URL=https://github.com/Sbosvk/digibyte-desktop-install/raw/main/icon.png
 
-# Image path check/setup
+# Check for icon path
 echo "Checking for icon path.."
-sleep 2
 if ! test -d "$ICON_DIR"; then
-    echo -e "${blue}Creating icon directory${reset} => {uline}$ICON_DIR${reset}"
-    mkdir $ICON_DIR
-    sleep 1
+        echo -e "${blue}Creating icon directory${reset} => ${uline}$ICON_DIR${reset}"
+        mkdir $ICON_DIR
 fi
-echo -e "Icon path ${green}OK${reset}"
-sleep 2
 
-# Image check/setup
-echo "Checking for icon.."
-sleep 2
-if ! test -f "$ICON"; then
-    echo -e "${red}No icon found${reset}"
-    sleep 1
-    echo -e "${blue}Fetching icon to${reset} ${uline}$ICON_DIR/icon.png${reset}${blue}..${reset}"
-    sleep 2
-    wget -P $ICON_DIR -O icon.png $ICON_URL
-else
-    echo -e "${blue}Icon found, relocating to${reset} ${uline}$ICON_DIR${reset}.."
-    mv $ICON $ICON_DIR/icon.png
-    sleep 2
+if ! test -f "$ICON_DIR/$ICON"; then
+    echo -e "${blue}Fetching icon to${reset} ${uline}$ICON_DIR/$ICON${reset}${blue}..${reset}"
+    wget -P $ICON_DIR $ICON_URL
 fi
-echo -e "Icon ${green}OK${reset}"
-sleep 1
+
+
+if test -f "$ICON_DIR/$ICON"; then
+        echo -e "Icon ${green}OK${reset}"
+fi
 
 # Create desktop entry file
 DESK_FILE=$APP.desktop
@@ -75,23 +97,23 @@ echo "Comment=Official DigiByte Core Hot Wallet for Linux" >>$DESK_FILE
 echo "Categories=Crypto;Network;Qt;Blockchain;Decentralized" >>$DESK_FILE
 echo "Terminal=false" >>$DESK_FILE
 echo "Exec=$APP" >>$DESK_FILE
-echo "Icon=$ICON" >>$DESK_FILE
+echo "Icon=$ICON_DIR/$ICON" >>$DESK_FILE
 
 echo -e "${blue}Installing to Application browser..${reset}"
 xdg-desktop-menu install $DESK_FILE
 xdg-desktop-menu forceupdate
 echo ""
-sleep 2
 
 echo -e "${blue}Cleaning up..${reset}"
 unset APP
+unset CURR_PATH
+unset PATH_SELECTION
 unset APP_PATH
 unset ICON
 unset ICON_DIR
 unset ICON_URL
 rm $DESK_FILE
 unset DESK_FILE
-sleep 2
 
 echo ""
 
